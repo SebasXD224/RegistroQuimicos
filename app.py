a2f716282edcfb58cbccdff19be1ffd5dce03d5e
Sebas_Azpe <azpeytiagael@gmail.com>
2025-11-04 22:17:57 -0600

    v1. Pagina Quimica - Estructura Inicial - Conexion a DB

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+import sqlite3
+import os
+
+DATABASE_PATH = os.path.join('static','databases','dataChemical.db')
+
+def get_connection():
+    conn = sqlite3.connect(DATABASE_PATH)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+app = Flask(__name__)
+
+@app.route('/')
+def records():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM Records ORDER BY id_record DESC")
+    records = cursor.fetchall()
+    
+    conn.close()
+    
+    return render_template('records.html',records=records)
+
+@app.route('/addRecord')
+def addRecord():
+    return render_template('addRecord.html')
+
+@app.route('/scanner')
+def scanner():
+    return render_template('scanner.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
