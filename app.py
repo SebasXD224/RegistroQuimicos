c9912d127e732b6afed93215ed2dad68ad534665
Sebas Azpeytia <azpeytiagael@gmail.com>
2025-11-05 08:03:45 -0600

    v1.1 - Wireframe "Records" Updated - Insertion of data in the database

--- app.py
+++ app.py
@@ -2,33 +2,37 @@ from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 import os
 
-DATABASE_PATH = os.path.join('static','databases','dataChemical.db')
+DATABASE_PATH = os.path.join("static", "databases", "dataChemical.db")
+
 
 def get_connection():
     conn = sqlite3.connect(DATABASE_PATH)
     conn.row_factory = sqlite3.Row
     return conn
 
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def records():
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM Records ORDER BY id_record DESC")
-    records = cursor.fetchall()
-    
-    conn.close()
-    
-    return render_template('records.html',records=records)
+    with get_connection() as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM Record ORDER BY id_record DESC")
+        records = cursor.fetchall()
 
-@app.route('/addRecord')
+    return render_template("records.html", records=records)
+
+
+@app.route("/addRecord")
 def addRecord():
-    return render_template('addRecord.html')
+    return render_template("addRecord.html")
 
-@app.route('/scanner')
+
+@app.route("/scanner")
 def scanner():
-    return render_template('scanner.html')
+    return render_template("scanner.html")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
